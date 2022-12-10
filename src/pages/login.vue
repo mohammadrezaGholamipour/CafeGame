fc820e9a2d54b03556dbd46a0c920bf32b3e0b79
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2022-12-10 22:35:50 +0330

    fix

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,21 +1,26 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive } from "vue";
 
 const state = reactive({
   lgoinInput: [
-    { placeholder: 'نام کاربری', type: 'text', value: '' },
-    { placeholder: 'رمز عبور', type: 'password', value: '' },
-  ]
-}) 
+    { placeholder: "نام کاربری", type: "text", value: "" },
+    { placeholder: "رمز عبور", type: "password", value: "" },
+  ],
+});
 </script>
 <template>
   <div class="ParentLogin">
     <div class="login">
-      <img class="LoginLogo" src="../assets/image/logo.png" alt="لوگو">
-      <input v-for="items in state.lgoinInput" class="LoginInput" :type="items.type" :placeholder="items.placeholder"
-        v-model="items.value">
-
+      <img class="LoginLogo" src="../assets/image/logo.png" alt="لوگو" />
+      <input
+        v-for="items in state.lgoinInput"
+        :placeholder="items.placeholder"
+        v-model="items.value"
+        class="LoginInput"
+        :type="items.type"
+      />
+      <button class="lgoinBtn">وارد شدن</button>
     </div>
   </div>
 </template>
-
+<style scoped></style>
