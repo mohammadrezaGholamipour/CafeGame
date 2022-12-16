2e35c278e09821799d4d1901dc922693078de30c
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2022-12-16 20:00:02 +0330

    login

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
+import VueTransitions from '@morev/vue-transitions';
+import '@morev/vue-transitions/styles';
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 import './css/index.css'
+
 //////////////////////
 const app = createApp(App)
 ///////////////
+app.use(VueTransitions)
 app.use(router)
+///////////////
 app.mount('#app')
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,26 +1,81 @@
 <script setup>
 import { reactive } from "vue";
-
+////////////////////////////
 const state = reactive({
-  lgoinInput: [
-    { placeholder: "نام کاربری", type: "text", value: "" },
-    { placeholder: "رمز عبور", type: "password", value: "" },
+  loginInput: [
+    {
+      placeholder: "نام کاربری",
+      name: "userName",
+      validate: false,
+      type: "text",
+      message: "",
+      value: "",
+    },
+    {
+      placeholder: "رمز عبور",
+      name: "passWord",
+      type: "password",
+      validate: false,
+      message: "",
+      value: "",
+    },
   ],
 });
+//////////////////////////////////////
+const handelClassInput = (name) => {};
+//////////////////////////////////////
+const handleValidateInput = (name) => {
+  const input = state.loginInput.filter((items) => items.name === name);
+  if (input[0].value.length > 0) {
+    if (input[0].name === "userName") {
+      if (isNaN(input[0].value)) {
+        if (input[0].value.length > 5) {
+          input[0].validate = true;
+        } else {
+          input[0].validate = false;
+          input[0].message = "باید بیشتر از 5 رقم باشد";
+        }
+      } else {
+        input[0].validate = false;
+        input[0].message = "نام کاربری نامعتبر میباشد";
+      }
+    } else {
+      if (input[0].value.length > 5) {
+        input[0].validate = true;
+      } else {
+        input[0].validate = false;
+        input[0].message = "باید بیشتر از 5 رقم باشد";
+      }
+    }
+  } else {
+    input[0].message = "";
+  }
+  //////////////////////////////////////
+};
 </script>
 <template>
   <div class="ParentLogin">
     <div class="login">
       <img class="LoginLogo" src="../assets/image/logo.png" alt="لوگو" />
-      <input
-        v-for="items in state.lgoinInput"
-        :placeholder="items.placeholder"
-        v-model="items.value"
-        class="LoginInput"
-        :type="items.type"
-      />
-     <button class="loginBtn">وارد شدن</button>
+      <template v-for="items in state.loginInput">
+        <input
+          @blur="handleValidateInput(items.name)"
+          class="LoginInput"
+          :placeholder="items.placeholder"
+          v-model="items.value"
+          :type="items.type"
+          maxlength="10"
+        />
+        <transition-scale>
+          <p
+            class="mt-2 bg-red-700 px-7 p-2 rounded-xl text-white"
+            v-if="items.message"
+          >
+            {{ items.message }}
+          </p>
+        </transition-scale>
+      </template>
+      <button @click="handleLogin" class="loginBtn">وارد شدن</button>
     </div>
   </div>
 </template>
-
